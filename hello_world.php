f36b344bfecc35ebb36799e0eda7d749693e1c9b
Aka Fadila <akafadila09@gmail.com>
2024-07-08 14:52:28 +0700

    First

---
+++ hello_world.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title><?php echo "Belajar PHP" ?></title>
+</head>
+
+<body>
+    <?php
+    echo "Hai, Guys! <br>";
+    echo "saya sedang belajar PHP<br>";
+    echo "<p>Sedang Belajar PHP</p>";
+    ?>
+    <?php
+    $nama = "petanikode";
+    $NAMA = "dian";
+    ?>
+    <?php
+    // ini adalah komentar
+    echo "Hello world";
+    /*
+    ini adalah komentar 
+    yang lebih dari satu 
+    baris
+    */
+    ?>
+    <?php
+    $umur = 15;
+    if ($umur > 18) {
+        echo "Kamu sudah dewasa";
+    }
+    ?>
+</body>
+
+</html>
---
+++ output_function.php
@@ -0,0 +1,22 @@
+<?php
+echo "<h2>Belajar PHP itu mudah!</h2>";
+echo ("Hello world!<br>");
+echo "Aku sedang belajar PHP!<br>";
+echo "Ini ", "teks ", "yang ", "dibuat ", "terpisah.";
+
+print "<h2>Belajar PHP (PRINT)!</h2>";
+print "Belajar mencetak teks print di PHP! <br>";
+
+echo "1" + "1"; // akan menghasilkan 2
+echo "<br>";
+echo "1" . "1"; // akan menghasilkan 11
+
+$txt1 = "Tutorial PHP";
+$txt2 = "petanikode.com";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Aku belajar pemrograman PHP di " . $txt2 . "<br>";
+echo $x + $y;
+?>
